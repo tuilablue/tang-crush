57319a283f8c461b94358f4705b251a8a739d4b6
tuilablue <85295631+tuilablue@users.noreply.github.com>
2021-06-03 17:03:45 +0100

    Update config.js

--- config.js
+++ config.js
@@ -1,13 +1,13 @@
 const CONFIG = {
-    introTitle: 'Babe à!',
+    introTitle: 'Hân à',
     introDesc: `Trái đất vốn lạ thường
     Mà sao em cứ đi nhầm đường
     Lạc vào tim anh lẻ loi
     Đằng sau chữ yêu đây là thương`,
     btnIntro: 'hihi',
-    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 😙',
-    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Vẫn cứ là thích anh <33',
+    title: 'Anh... Vẫn còn yêu em...',
+    desc: 'Mình quay lại được không? Anh hứa sẽ không làm em buồn nữa đâu :( ',
+    btnYes: 'Dạ...',
     btnNo: 'Không, Anh trai à :3',
     question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
     btnReply: 'Gửi cho anh <3',
@@ -15,5 +15,5 @@ const CONFIG = {
     mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
     messDesc: 'Tối nay 7h anh qua đón nhé công chúa.',
     btnAccept: 'Okiiiii lun <3',
-    messLink: 'http://fb.com' //link mess của các bạn. VD: https://m.me/nam.nodemy
+
 }
